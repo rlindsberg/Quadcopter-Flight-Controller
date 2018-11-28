4fe820a2da099cb1934630f78b2617a4533b3fab
nessnah198 <37583243+nessnah198@users.noreply.github.com>
2018-11-28 15:43:03 +0100

    Uppdaterat analys.c
    kommentarer osv.

--- Src/analys.c
+++ Src/analys.c
@@ -26,15 +26,12 @@
 #include "gyroscope_l3gd20h.h"
 #include <stdio.h>
 
-//#define MAX_ANALYSE_CHANNELS 4
-
 /* External variables --------------------------------------------------------*/
 extern UART_HandleTypeDef huart3;
 extern const portTickType MAIN_FREQUENCY;
 //For test purposes
 uint32_t gAnalysWDCheckback=0;
 
-//static int16_t val[MAX_ANALYSE_CHANNELS];
 
 /* Thread definitions --------------------------------------------------------*/
 /** ****************************************************************************
@@ -46,58 +43,32 @@ uint32_t gAnalysWDCheckback=0;
 *******************************************************************************/
 void StartAnalysTask(void const * argument)
 { 
+  // Get current time
   portTickType last_task_start = xTaskGetTickCount();
   
+  // Fetch data from mailboxes
   osEvent check_mail = osMailGet(sensorFilter_mailbox, osWaitForever);
   FILTER_complement_struct *filter_data = (FILTER_complement_struct*)check_mail.value.p;
   
   osEvent check_mail2 = osMailGet(analys_mailbox, osWaitForever);
   main_struct *control_data = (main_struct*)check_mail2.value.p;
   
-//    ACC_TypeDef acc_values;
-//    GYR_TypeDef gyr_values;
   
-  
-  //Delay på 10sekunder för att hinna pair:a från datorn till quadens analys-bluetooth
-  vTaskDelay(10000); // (kommentera bort denna rad om bluetooth inte används)
-  
+  //***************BLUETOOTH******************
+  //A 10 sec delay on the thread to have time to connect to Bluetooth. Otherwise not necessary.
+  vTaskDelay(10000);
+  //******************************************
   
   /* Thread */
   while(1){
     
-//    ACC_update_xyz();
-//    GYR_update_xyz();
-//    
-//    val[0] = acc_values.x_raw;
-//    val[1] = acc_values.y_raw;
-//    val[2] = acc_values.z_raw;
-//    val[3] = gyr_values.x_raw;
-//    val[4] = gyr_values.y_raw;
-//    val[5] = gyr_values.z_raw;
-//    val[6] = xTaskGetTickCount();
+
     
-    
-    // Send data with UART.
-//    float val[11];
-//    uint8_t ctr = 0;
-//    val[ctr++] = xTaskGetTickCount();
-//    val[ctr++] = lowpass_data_acc_x.output;
-//    val[ctr++] = lowpass_data_acc_y.output;
-//    val[ctr++] = lowpass_data_acc_z.output; 
-//    val[ctr++] = acc_raw.x_raw;
-//    val[ctr++] = acc_raw.z_raw;
-//    val[ctr++] = acc_raw.y_raw;
-//    val[ctr++] = complement_data.filter_pitch;
-//    val[ctr++] = complement_data.filter_roll;
-//    val[ctr++] = complement_data.acc_pitch;
-//    val[ctr++] = complement_data.acc_roll;
-    
-    
-    //might have to use "->"
-    //do this with loop?
     float val[23];
     uint8_t ctr = 0;
-    //val[ctr++] = xTaskGetTickCount();
+    
+    
+    //Filter data
     val[ctr++] = filter_data->acc_x;
     val[ctr++] = filter_data->acc_y;
     val[ctr++] = filter_data->acc_z;
@@ -111,31 +82,31 @@ void StartAnalysTask(void const * argument)
     val[ctr++] = filter_data->filter_yaw;
     
     
-    //från PID:n
-    val[ctr++] = control_data->PIDoutputGyroYaw.f;    //12        //Change name of errorgyroyaw
-    val[ctr++] = control_data->PIDoutputPitch.f;  //13
-    val[ctr++] = control_data->PIDoutputRoll.f;   //14
+    //Control data
+    val[ctr++] = control_data->PIDoutputGyroYaw.f;       //TODO:Change name of errorgyroyaw
+    val[ctr++] = control_data->PIDoutputPitch.f;
+    val[ctr++] = control_data->PIDoutputRoll.f;
     
    
-    //motorerna
-    val[ctr++] = control_data->RFmotor.f;         //15
-    val[ctr++] = control_data->LFmotor.f;         //16
-    val[ctr++] = control_data->RBmotor.f;         //17
-    val[ctr++] = control_data->LBmotor.f;         //18
+    //Motors
+    val[ctr++] = control_data->RFmotor.f;
+    val[ctr++] = control_data->LFmotor.f;
+    val[ctr++] = control_data->RBmotor.f;
+    val[ctr++] = control_data->LBmotor.f;
     
     
-    //från handkontrollern
-    val[ctr++] = control_data->yaw.f;             //19
-    val[ctr++] = control_data->pitch.f;           //20
-    val[ctr++] = control_data->roll.f;            //21
-    val[ctr++] = control_data->thrust;          //22
-    val[ctr++] = control_data->emergency;       //23
+    //Remote
+    val[ctr++] = control_data->yaw.f;
+    val[ctr++] = control_data->pitch.f;
+    val[ctr++] = control_data->roll.f;
+    val[ctr++] = control_data->thrust;
+    val[ctr++] = control_data->emergency;
      
     
     // Transmit signals to UART peripheral
     HAL_UART_Transmit(&huart3,(uint8_t*)&val, sizeof(float)*ctr, 4);
     huart3.State = HAL_UART_STATE_READY;
-    
+
     
     // Sleep thread until end of hyperperiod
     vTaskDelayUntil(&last_task_start,MAIN_FREQUENCY); 
